f9747b555bcce5726f58921572a52aedd17aa989
Bruno Bueno <brunobuenoferreira98@gmail.com>
2022-11-26 02:50:43 -0300

    first commit

---
+++ src/components/body.js
@@ -0,0 +1,13 @@
+import './styles/styleBody.css'
+// import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+function Body() {
+    return (
+        <div className="createList">
+            <Button variant="contained">Criar lista</Button>
+        </div>
+    );
+}
+
+export default Body;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import './styles/styleHeader.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>WORK TO-DO LIST</h1>
+            <h4>Clique no botão a baixo para criar sua lista</h4>
+        </div>
+    );
+}
+
+export default Header;
